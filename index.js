5ff6e4299b14c5efe24efcd67e9e946a449d8d5c
Helen Liutongco <bellpickle@gmail.com>
2017-10-29 20:33:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,3 +22,22 @@ function addToCart(items) {
   console.log(`${items} has been added to your cart.`);
   return cart;
 }
+
+function viewCart() {
+var array=[];
+if(cart<=[]) {
+  console.log("Your shopping cart is empty.");
+}
+  for(var i=0;i<cart.length;i++) {
+    array.push(` ${cartItems[i]} at $${cartPrice[i]}`);
+  }
+  if(cart.length===1) {
+    console.log(`In your cart, you have${array}.`);
+  }
+  else if(cart.length===2) {
+    console.log(`In your cart, you have${array[0]} and${array[1]}.`);
+  }
+  else {
+    console.log(`In your cart, you have${array.slice(0,-1)}, and${array.slice(-1)}.`);
+  }
+}
