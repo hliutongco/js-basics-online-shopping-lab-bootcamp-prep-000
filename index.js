e13f13f12993aae49c9f2e999d1b148c352c9637
Helen Liutongco <bellpickle@gmail.com>
2017-10-29 19:50:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,52 +16,8 @@ function addToCart(items) {
   var num=Math.random()*100;
   var itemPrice=Math.floor(num);
   cartItems.push(items);
-  Object.assign(cart, { [items]:itemPrice });
+  var newObject={ [items]:itemPrice };
+  Object.assign(cart, newObject);
   console.log(`${items} has been added to your cart.`);
   return cart;
-}
-
-function viewCart() {
-var array=[];
-if(cart<=[]) {
-  console.log("Your shopping cart is empty.");
-}
-  for(var i=0;i<cart.length;i++) {
-    array.push(` ${cart[i]} at $${cartPrice[cart[i]]}`);
-  }
-console.log(`In your cart, you have${array.slice(0,-1)}, and${array.slice(-1)}.`);
-}
-
-
-function total() {
-  var totalPrice=0;
-  for(var i=0;i<cart.length;i++) {
-    totalPrice+=cartPrice[cart[i]];
-  }
-}
-
-function removeFromCart(itemName) {
-var counter=0;
-  for(var i=0;i<cart.length;i++) {
-    if(itemName===cart[i]) {
-      delete cartPrice[cart[i]];
-      cart.splice(i,1);
-      return cart;
-    }
-  }
-  return cart;
-}
-
-function placeOrder(creditCard) {
-  if(!creditCard) {
-    console.log("Sorry, we don't have a credit card on file for you.");
-  }
-  else {
-    console.log(`Your total cost is ${total()}, which will be charged to the card ${creditCard}.`);
-    for(var i=cart.length;i>0;i--) {
-      delete cartPrice[cart[i-1]];
-      cart.pop();
-    }
-    return cart;
-  }
 }
