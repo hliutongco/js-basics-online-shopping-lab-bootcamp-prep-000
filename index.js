fa5b2f2d038e9ac11c0df20f282a742a9d53c56f
Helen Liutongco <bellpickle@gmail.com>
2017-10-30 04:10:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,15 +8,11 @@ function setCart(c) {
   return cart;
 }
 
-var cartItems=[];
-var cartPrice=[];
 var cart=[];
 
 function addToCart(items) {
   var num=Math.random()*100;
   var itemPrice=Math.floor(num);
-  cartItems.push(items);
-  cartPrice.push(itemPrice);
   var newObject={ [items]:itemPrice };
   cart.push(newObject);
   console.log(`${items} has been added to your cart.`);
