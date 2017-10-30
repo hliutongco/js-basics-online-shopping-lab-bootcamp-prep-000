e232980652296030956f9f915569bf2159f93fd0
Helen Liutongco <bellpickle@gmail.com>
2017-10-30 05:18:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ if(cart<=[]) {
 
 function removeFromCart(itemName) {
   for(var i=0;i<cart.length;i++) {
-    if(cart[i].hasOwnProperty(cartItems[i])) {
+    if(cart[i].hasOwnProperty(itemName)) {
       cart.splice(i,1);
       return cart;
     }
