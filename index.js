48e921587a9330bdb0e57bda004eb30f2f3b48fe
Helen Liutongco <bellpickle@gmail.com>
2017-10-30 05:28:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -64,15 +64,11 @@ function removeFromCart(itemName) {
 
 
 function placeOrder(cardNumber) {
-  var totalPrice=0;
-  for(var i=0;i<cart.length;i++) {
-    totalPrice+=cart[i][cartItems[i]];
-  }
   if(!cardNumber) {
     console.log("Sorry, we don't have a credit card on file for you.");
   }
   else {
-    console.log(`Your total cost is $${totalPrice}, which will be charged to the card ${cardNumber}.`);
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
     for(var i=cart.length;i>0;i--) {
       cart.pop();
     }
