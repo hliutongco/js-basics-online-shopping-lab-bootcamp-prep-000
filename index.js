b10c80469a4c7cef00c0e73b851b4304c0a7c102
Helen Liutongco <bellpickle@gmail.com>
2017-10-30 05:20:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,3 +61,17 @@ function removeFromCart(itemName) {
   console.log("That item is not in your cart.");
   return cart;
 }
+
+
+function placeOrder(creditCard) {
+  if(!creditCard) {
+    console.log("Sorry, we don't have a credit card on file for you.");
+  }
+  else {
+    console.log(`Your total cost is ${total()}, which will be charged to the card ${creditCard}.`);
+    for(var i=cart.length;i>0;i--) {
+      cart.pop();
+    }
+    return cart;
+  }
+}
